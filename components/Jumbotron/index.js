8e85550c33ec7d8897688397e7006ffaa489c141
matt <mpocock@me.com>
2018-07-04 16:48:56 +0100

    Added a jumbotron

---
+++ components/Jumbotron/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.scss';
+
+const Jumbotron = ({
+    children,
+    imgSrc,
+    imgAlt,
+    imgRadius,
+    ...props
+}) => (
+    <div
+        className={styles.Jumbotron}
+        {...props}
+    >
+        <div
+            className={styles.JumbotronInner}
+        >
+            <img
+                className={styles.JumbotronImage}
+                src={imgSrc}
+                alt={imgAlt}
+                height={imgRadius}
+                width={imgRadius}
+            />
+            <div>
+                {children}
+            </div>
+        </div>
+    </div>
+);
+
+Jumbotron.defaultProps = {
+    children: [],
+};
+
+Jumbotron.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.object,
+        PropTypes.array,
+    ]),
+    imgSrc: PropTypes.string.isRequired,
+    imgAlt: PropTypes.string.isRequired,
+    imgRadius: PropTypes.number.isRequired,
+};
+
+export default Jumbotron;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import BackdropInsetShadow from '../components/BackdropInsetShadow';
 // import { linkTo } from '@storybook/addon-links';
 import './resets.scss';
+import Jumbotron from '../components/Jumbotron';
 
 storiesOf('BackdropInsetShadow')
   .add('Default', () => (
@@ -18,4 +19,39 @@ storiesOf('BackdropInsetShadow')
         Hello World
       </h1>
     </BackdropInsetShadow>
+  ));
+
+storiesOf('Jumbotron')
+  .add('Default', () => (
+    <Jumbotron
+      imgSrc="https://avatars1.githubusercontent.com/u/28293365?s=460&v=4"
+      imgAlt="Matt Pocock"
+      imgRadius={220}
+    >
+      <h1
+        style={{ marginBottom: 0 }}
+      >
+        Matt Pocock
+      </h1>
+      <h3>A marvellous web developer</h3>
+    </Jumbotron>
+  ))
+  .add('With Custom background', () => (
+    <Jumbotron
+      imgSrc="https://avatars1.githubusercontent.com/u/28293365?s=460&v=4"
+      imgAlt="Matt Pocock"
+      imgRadius={220}
+      style={{
+        background: '#001f3f',
+        boxShadow: '0 0 150px inset rgba(0,0,0,0.15)',
+        color: 'hsla(210, 100%, 75%, 1.0)',
+      }}
+    >
+      <h1
+        style={{ marginBottom: 0 }}
+      >
+        Matt Pocock
+      </h1>
+      <h3>A marvellous web developer</h3>
+    </Jumbotron>
   ));
